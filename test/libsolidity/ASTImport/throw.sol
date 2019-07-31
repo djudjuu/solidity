01780c41fff5197bd7530be831489eae6b139b44
djudjuu <julfaber@gmail.com>
2019-07-31 10:27:48 +0200

    found bug for revert/assert/require

--- test/libsolidity/ASTImport/throw.sol
+++ test/libsolidity/ASTImport/throw.sol
@@ -1,8 +1,10 @@
 contract C {
   function doNotAskForSevens(uint x) public pure returns(bool) {
-    require(x != 7, "You must NOT ask for seven");
-    assert(x != 77);
-    if (x == 777) { revert(); }
+    // breaking!
+    // wrong referenced/overloaded declarations
+    /* require(x != 7, "You must NOT ask for seven"); */
+    /* assert(x != 77); */
+    /* if (x == 777) { revert(); } */
     return true;
   }
 }
